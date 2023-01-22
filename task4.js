8c13d9909cd2019cf28e04e59b8db69cc481b500
Yulia Shaginian <juliashagi@gmail.com>
2023-01-22 19:01:39 +0300

    added a draft solution of the 4th task

--- task4.js
+++ task4.js
@@ -6,11 +6,18 @@ const users = [
    , {id: 4, posts: [{id: 41, tags: ["CSS"]}, {id: 42, tags: ["Python"]}, {id: 43, tags: []}]}
   ]
   
-  
-  
-  /* сначала нужно создать переменную для каждого пользователя,
-  затем найти всех у кого в тэгах есть PHP и вернуть их */
-  
- let usersId = users.map(item => item.id);
- const usersThatPostedAboutPHP = usersId.filter(item => ('tags' == 'PHP') ? 1 : -1);
+ 
+let arr1 = {id: 1, posts: [{id: 11, tags: ["PHP"]}, {id: 12, tags: []}, {id: 13, tags: ["JS"]}]};
+let arr2 = {id: 2, posts: [{id: 21, tags: []}, {id: 22, tags: ["Python", "PHP"]}, {id: 23, tags: []}]};
+let arr3 = {id: 3, posts: [{id: 31, tags: ["PHP"]}, {id: 32, tags: []}, {id: 33, tags: []}]};
+let arr4 = {id: 4, posts: [{id: 41, tags: ["CSS"]}, {id: 42, tags: ["Python"]}, {id: 43, tags: []}]};
+let user1 = Array.from(arr1);
+let user2 = Array.from(arr2);
+let user3 = Array.from(arr3);
+let user4 = Array.from(arr4);
+let str = `${user1}, ${user2}, ${user3}, ${user4}`;
+let allusers = str.split(', ');
+filter(allusers, (item) => item.includes('PHP'));
+
+
 
