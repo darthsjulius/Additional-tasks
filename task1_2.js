fd42e7abb48c5e71cff02fd9763d365cbaf29fbc
Yulia Shaginian <juliashagi@gmail.com>
2023-02-14 17:50:37 +0300

    optimized the recursion in task 1.2

--- task1_2.js
+++ task1_2.js
@@ -1,11 +1,7 @@
 function geometricProgression1(n) {
     let b = 1;
     let q = 2;
-    if (n == 1) {
-        return q;
-    } else {
-        return b * (q ** (n - 1));
-    }
+    return (n == 1) ? q : b * (q ** (n - 1)); 
 }
 
 console.log(geometricProgression(8)); // 128
